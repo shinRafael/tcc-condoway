e237b2ec2fcdb357ecf19cfe2b460902530bb625
jaozinp1 <joaohenriqeu23@gmail.com>
2025-10-07 19:34:43 -0300

    aaa
    aaa

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,11 @@ import axios from 'axios';
 
 // Crie a instância do Axios com a URL base
 const api = axios.create({
+<<<<<<< Updated upstream
   baseURL: 'http://localhost:3333',
+=======
+  baseURL: "http://localhost:3333",
+>>>>>>> Stashed changes
 });
 
 // Adicione o interceptor de requisições
